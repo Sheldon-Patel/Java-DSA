e8435384729b7a368db4af937055df9d8ec245d5
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-06-04 22:54:37 +0530

    initial

--- src/Lec07_Operators/operators.java
+++ src/Lec07_Operators/operators.java
@@ -1,4 +1,42 @@
 package Lec07_Operators;
 
 public class operators {
+
+    public static void main() {
+// arithmetic operator
+        int solvedThisWeek = 25;
+        int solvedLastWeek = 15;
+
+        int total = solvedThisWeek + solvedLastWeek;
+        int difference = solvedThisWeek - solvedLastWeek;
+        int projected = solvedThisWeek * 4;
+        int average = solvedThisWeek / 7;
+        int remainder = solvedThisWeek % 7;
+
+        System.out.println(total);
+        System.out.println(difference);
+        System.out.println(projected);
+        System.out.println(average);
+        System.out.println(remainder);
+
+// relational operator
+        int currentStreak = 45;
+        int targetStreak = 50;
+
+        System.out.println(currentStreak == targetStreak);
+        System.out.println(currentStreak != targetStreak);
+        System.out.println(currentStreak > targetStreak);
+        System.out.println(currentStreak < targetStreak);
+        System.out.println(currentStreak >= targetStreak);
+        System.out.println(currentStreak <= targetStreak);
+
+        // logical operation
+        boolean completedDSA = true;
+        boolean completedCore = false;
+
+        System.out.println(completedDSA && completedCore);
+        System.out.println(completedDSA || completedCore);
+        System.out.println(!completedCore);
+    }
 }
+
