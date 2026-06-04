6ccc9def94f5a6e443e79fca91f29d1d370aa805
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-06-04 23:43:50 +0530

    initial

--- src/Lec08_Input_Output/TakingInput.java
+++ src/Lec08_Input_Output/TakingInput.java
@@ -1,5 +1,6 @@
 package Lec08_Input_Output;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class TakingInput {
@@ -9,6 +10,19 @@ public class TakingInput {
 //        int b = 1;
 //        System.out.println(a+b);
 
-        Scanner
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the value for first num:");
+        int firstNum = sc.nextInt();
+        System.out.println("Enter the value for second num");
+        int secondNum = sc.nextInt();
+        int ans = firstNum + secondNum;
+        System.out.println("ans:"+ans);
+
+        BigInteger bg = sc.nextBigInteger();
+        System.out.println("big integer:"+bg);
+
+        boolean flag = sc.nextBoolean();
+        short shortVal = sc.nextShort();
+        System.out.println(" ");
     }
 }
