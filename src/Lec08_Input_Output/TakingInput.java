25e7bee585e88f42179e702115cf1d6b98b22cea
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-06-04 23:29:01 +0530

    initial

---
+++ src/Lec08_Input_Output/TakingInput.java
@@ -0,0 +1,14 @@
+package Lec08_Input_Output;
+
+import java.util.Scanner;
+
+public class TakingInput {
+    static void main() {
+
+//        int a = 5;
+//        int b = 1;
+//        System.out.println(a+b);
+
+        Scanner
+    }
+}
