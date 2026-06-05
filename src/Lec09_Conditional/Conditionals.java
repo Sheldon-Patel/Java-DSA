0b587ca45801c83dce576760d88a69b6ec356554
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-06-05 23:18:16 +0530

    initial

--- src/Lec09_Conditional/Conditionals.java
+++ src/Lec09_Conditional/Conditionals.java
@@ -50,6 +50,30 @@ public class Conditionals {
         else {
             System.out.println("Needs Improvement");
         }
+
+        int day=3;
+        if(day==1){
+            System.out.println("monday");
+        }
+        else if (day == 2) {
+            System.out.println("tuesday");
+        }
+        else if (day == 3) {
+            System.out.println("wednesday");
+        }
+        else if (day == 4) {
+            System.out.println("thursday");
+        }
+        else if (day == 5) {
+            System.out.println("friday ");
+        }
+        else if (day == 6) {
+            System.out.println("saturday");
+        }
+        else {
+            System.out.println("sunday");
+        }
+        System.out.println("abb ma line 76 par aaya hu");
     }
 }
 
