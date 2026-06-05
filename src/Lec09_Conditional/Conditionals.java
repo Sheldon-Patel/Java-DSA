92c71ec7d2fc92241f2e696a63c8c6e9cbf3c551
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-06-05 23:54:25 +0530

    initial

--- src/Lec09_Conditional/Conditionals.java
+++ src/Lec09_Conditional/Conditionals.java
@@ -85,16 +85,22 @@ public class Conditionals {
         switch (day1){
             case 1:
                 System.out.println("monday");
+                break;
             case 2:
                 System.out.println("tue");
+                break;
             case 3:
                 System.out.println("wed");
+                break;
             case 4:
                 System.out.println("thu");
+                break;
             case 5:
                 System.out.println("fri");
+                break;
             case 6:
                 System.out.println("sat");
+                break;
             default:
                 System.out.println("sunday");
         }
