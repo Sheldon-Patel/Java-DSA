12ff5cd024153bef2abf5935484917afb37b8199
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-06-05 23:03:13 +0530

    initial

--- src/Lec09_Conditional/Conditionals.java
+++ src/Lec09_Conditional/Conditionals.java
@@ -7,12 +7,22 @@ public class Conditionals {
 
         if (dailyPractice >= 10) {
             System.out.println("Good consistency!");
-
-            int age =100;
-            if(age>=18){
-                System.out.println("you can vote");
-            }
         }
+
+        int age =100;
+        if(age>=18){
+            System.out.println("you can vote");
+        }
+
+        // if-else statement
+        int score = 42;
+
+        if (score >= 50) {
+            System.out.println("Pass");
+        } else {
+            System.out.println("Fail");
+        }
+
     }
 }
 
