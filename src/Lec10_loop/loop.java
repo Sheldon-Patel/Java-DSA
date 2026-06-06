fab8eabbaaa83de64f5227d0e046d911e70af7f2
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-06-06 22:27:23 +0530

    initial

--- src/Lec10_loop/loop.java
+++ src/Lec10_loop/loop.java
@@ -1,4 +1,14 @@
 package Lec10_loop;
 
 public class loop {
+    static void main() {
+        //first loop
+         for (int i=1; i<=5; i++){
+             System.out.println("val of i:"+i) ;
+         }
+
+         for (int a=1; a<=110; a++){
+             System.out.println("love babbar");
+         }
+    }
 }
