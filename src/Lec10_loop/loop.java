d48b4f03103fbd29e4e6d6b2149d239273209b28
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-06-06 22:47:22 +0530

    initial

--- src/Lec10_loop/loop.java
+++ src/Lec10_loop/loop.java
@@ -2,21 +2,28 @@ package Lec10_loop;
 
 public class loop {
     static void main() {
-        //first loop
-         for (int i=1; i<=5; i++){
-             System.out.println("val of i:"+i) ;
-         }
+//        //first loop
+//         for (int i=1; i<=5; i++){
+//             System.out.println("val of i:"+i) ;
+//         }
+//
+//         for (int a=1; a<=10; a++){
+//             System.out.println("love babbar");
+//         }
+//
+//         for(int j=1; j<=20; j+=2){
+//             System.out.println(j);
+//         }
+//
+//        for(int j=2; j<=20; j+=2){
+//            System.out.println(j);
+//        }
 
-         for (int a=1; a<=10; a++){
-             System.out.println("love babbar");
-         }
-
-         for(int j=1; j<=20; j+=2){
-             System.out.println(j);
-         }
-
-        for(int j=2; j<=20; j+=2){
-            System.out.println(j);
+        for(int i=1;i<=3;i++){ // row
+            for (int j=1; j<=3; j++){ // coloumns
+                System.out.print("* ");
+            }
+            System.out.println();// move to next line
         }
     }
 }
