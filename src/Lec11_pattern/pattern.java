ba4bd806971d01fe61738e5d24c2fc3d3f32cd6f
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-06-08 22:58:00 +0530

    initial

--- src/Lec11_pattern/pattern.java
+++ src/Lec11_pattern/pattern.java
@@ -15,9 +15,21 @@ public class pattern {
 //        }
 
         // pattern 2: solid rectangle
-            int n = 3;
+//            int n = 3;
+//            for(int row=1; row<=n; row++){
+//                // for each row-> 5 col
+//                for(int col=1; col<=5; col++){
+//                    System.out.print("* ");
+//                }
+//                System.out.println();
+//            }
+
+        // patern 3: right angle
+            int n=5;
             for(int row=1; row<=n; row++){
-                for(int col=1; col<=5; col++){
+                //for each row -> varable coloum
+                // formula-> col -> (1->value of row)
+                for(int col=1; col<=row;col++ ){
                     System.out.print("* ");
                 }
                 System.out.println();
