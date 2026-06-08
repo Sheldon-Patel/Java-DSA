1f19cfa959a83be5cab325aaf8513bf847b45e30
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-06-08 22:39:20 +0530

    initial

--- src/Lec11_pattern/pattern.java
+++ src/Lec11_pattern/pattern.java
@@ -1,4 +1,17 @@
 package Lec11_pattern;
 
 public class pattern {
+    static void main() {
+        int n=4;
+
+        for( int row=1; row<=n; row++){
+            //for each row -> n coloum
+            for (int col=1; col<=n; col++){
+                // print *
+                System.out.print("* ");
+            }
+            // move to next line or row
+            System.out.println();
+        }
+    }
 }
