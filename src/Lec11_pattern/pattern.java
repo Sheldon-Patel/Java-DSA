5886b7f677cee0bdfe087c60723130554465c772
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-06-08 22:47:35 +0530

    initial

--- src/Lec11_pattern/pattern.java
+++ src/Lec11_pattern/pattern.java
@@ -2,16 +2,25 @@ package Lec11_pattern;
 
 public class pattern {
     static void main() {
-        int n=4;
+    // pattern 1: solid square
+//        int n=4;
+//        for( int row=1; row<=n; row++){
+//            //for each row -> n coloum
+//            for (int col=1; col<=n; col++){
+//                // print *
+//                System.out.print("* ");
+//            }
+//            // move to next line or row
+//            System.out.println();
+//        }
 
-        for( int row=1; row<=n; row++){
-            //for each row -> n coloum
-            for (int col=1; col<=n; col++){
-                // print *
-                System.out.print("* ");
+        // pattern 2: solid rectangle
+            int n = 3;
+            for(int row=1; row<=n; row++){
+                for(int col=1; col<=5; col++){
+                    System.out.print("* ");
+                }
+                System.out.println();
             }
-            // move to next line or row
-            System.out.println();
-        }
     }
 }
