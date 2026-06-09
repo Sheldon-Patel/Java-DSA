6bc2be2629b7140753ab9812154c0b9abcde3746
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-06-09 22:23:10 +0530

    initial

--- src/Lec11_pattern/pattern.java
+++ src/Lec11_pattern/pattern.java
@@ -67,7 +67,14 @@ public class pattern {
         int n=5;
         for (int row=1; row<=n; row++) {
 //                 for each row -> variable coloum
-
+                    // spaces
+            for (int col=1; col<=n-row; col++) {
+                System.out.print(" ");
+            }
+            for (int col=1; col<=2*row-1; col++){
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
