04da4460331597860b6dd45eb682cc4a6133e115
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-06-09 22:32:48 +0530

    initial

--- src/Lec11_pattern/pattern.java
+++ src/Lec11_pattern/pattern.java
@@ -64,14 +64,31 @@ public class pattern {
 //        }
 
         //pattern 6:solid pyramid
+//        int n=5;
+//        for (int row=1; row<=n; row++) {
+////                 for each row -> variable coloum
+//                    // spaces
+//            for (int col=1; col<=n-row; col++) {
+//                System.out.print(" ");
+//            }
+        //star
+//            for (int col=1; col<=2*row-1; col++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+
+        //pattern 7:inverted solid pyramid
         int n=5;
         for (int row=1; row<=n; row++) {
 //                 for each row -> variable coloum
                     // spaces
-            for (int col=1; col<=n-row; col++) {
+            for (int col=1; col<=row-1; col++) {
                 System.out.print(" ");
             }
-            for (int col=1; col<=2*row-1; col++){
+            //star
+            for (int col=1; col<=2*n-2*row+1; col++){
                 System.out.print("*");
             }
             System.out.println();
