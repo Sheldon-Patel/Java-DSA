54fb202a6021f5e3228c0af869436a24b54d8b29
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-06-11 22:55:52 +0530

    initial

--- src/Lec11_pattern/pattern.java
+++ src/Lec11_pattern/pattern.java
@@ -415,8 +415,15 @@ public class pattern {
             }
             // part 2
             for (int col = 1; col <= row; col++) {
+                int a=col;
+                int b=('A'-1);
+                int ans=a+ b;
+                char finalAns = (char)ans;
+                System.out.print(finalAns+" ");
+            }
+            // part 3
+            for (int col = 1; col <=row-1; col++) {
                 System.out.print("* ");
-
             }
             System.out.println();
         }
