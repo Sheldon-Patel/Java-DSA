7ce5ea113c68cce874c5902c3a7d82b21370ae7a
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-06-11 22:25:49 +0530

    initial

--- src/Lec11_pattern/pattern.java
+++ src/Lec11_pattern/pattern.java
@@ -334,19 +334,37 @@ public class pattern {
 
 
         //pattern 16:right angle with alphabets
+//        int n=5;
+//
+//        for (int row=1; row<=n; row++) {
+//            // for each row -> variable columns
+//            for (int col = 1; col <= row; col++) {
+//                int a=col;
+//                int b=('A'-1);
+//                int ans=a+b;
+//                char finalAns = (char)ans;
+//                System.out.print(finalAns+" ");
+//            }
+//            System.out.println();
+//        }
+
+
+        //pattern 16:right angle with alphabets but inverted
         int n=5;
 
         for (int row=1; row<=n; row++) {
             // for each row -> variable columns
             for (int col = 1; col <= row; col++) {
-                int a=col;
-                int b=('A'-1);
+                int a=n-col;
+                int b= 'A'  ;
                 int ans=a+b;
                 char finalAns = (char)ans;
                 System.out.print(finalAns+" ");
             }
             System.out.println();
-        }
+
+            }
+
 
 
     }
