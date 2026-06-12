9a6b5161c4b61ae4bd2e01bb81234567b920200b
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-06-12 22:40:46 +0530

    initial

--- src/Lec12_Methods/Methods.java
+++ src/Lec12_Methods/Methods.java
@@ -1,4 +1,14 @@
 package Lec12_Methods;
 
 public class Methods {
+    void print2KaTable{
+        for (int i = 1; i <=10; i++) {
+            int ans= 2*i;
+            System.out.println(" -> "+ans);
+
+        }
+    }
+    static void main() {
+
+    }
 }
