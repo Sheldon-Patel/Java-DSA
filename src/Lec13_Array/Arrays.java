772c7c1f172296ca9167b05a6aeb0ac1fc5e080a
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-06-13 23:04:33 +0530

    initial

--- src/Lec13_Array/Arrays.java
+++ src/Lec13_Array/Arrays.java
@@ -10,7 +10,16 @@ public class Arrays {
         arr = new int[5];
 
         // initilization
-        int brr[] = {1,2,3 };
+        int brr[] = {10,20,30};
+
+        int n=brr.length;
+        for(int index=0; index<=n-1; index++){
+            System.out.println(brr[index]);
+        }
+
+        System.out.println("value at 0 index: "+brr[0]);
+        System.out.println("value at 1 index: "+brr[1]);
+        System.out.println("value at 2 index: "+brr[2]);
 
 
 
