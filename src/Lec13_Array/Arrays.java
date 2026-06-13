fb622a0103c9c8c49198b7bb895559e7271ebbb5
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-06-13 22:53:59 +0530

    initial

--- src/Lec13_Array/Arrays.java
+++ src/Lec13_Array/Arrays.java
@@ -2,6 +2,16 @@
 public class Arrays {
 }
 
-static void main() {
+    static void main() {
+    // declaration
+        int arr[];
+
+        //allocation     1 int take 4 byte so 4 int take 20 byte
+        arr = new int[5];
+
+        // initilization
+        int brr[] = {1,2,3 };
+
+
 
 }
