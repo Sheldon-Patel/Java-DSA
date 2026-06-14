8bfc2e190db63e2987636f4e7391216ad378c2fc
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-06-14 22:00:46 +0530

    initial

--- src/Lec13_Array/Arrays.java
+++ src/Lec13_Array/Arrays.java
@@ -4,6 +4,22 @@ public class Arrays {
 
     static void main() {
 
+    int arr[]={3,2,-5,21,10};
+     int n = arr.length;
+      int maxValue = arr[0];
+
+       // compare mavValue with every element in array
+    for(int i=0; i<=n-1; i++){
+        if(arr[i]>maxValue){
+            //update maxValue
+              maxValue = arr[i];
+        }
+    }
+
+        System.out.println(maxValue);
+
+
+
 //        int  arr[]={2,3,10,20};
 //        int ans =1;
 //        int n=arr.length;
