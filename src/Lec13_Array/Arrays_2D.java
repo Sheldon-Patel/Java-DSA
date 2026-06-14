132aa21e2bfb5f5090baae0e3c838bf1a0a1890b
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-06-14 23:00:02 +0530

    initial

--- src/Lec13_Array/Arrays_2D.java
+++ src/Lec13_Array/Arrays_2D.java
@@ -9,19 +9,45 @@ public class Arrays_2D {
     // allocation
     arr =new int[3][4];
     // init
-    int[][] brr = {
-                    {1, 2},
-                    {2, 3},
-                    {3, 4},
-                    {4, 5}
-                    };
+//    int[][] brr = {
+//                    {1, 2},
+//                    {2, 3},
+//                    {3, 4},
+//                    {4, 5}
+//                    };
+//
+////        System.out.println(brr[3][1 ]);
+//        int rowLength = brr.length;        // for row length of 2d array
+//        int colLength = brr[0].length;     // for col length of 2d array ,
+//         // each row have same number of col so assume any one row length to get col length
+//
+//        for(int rowIndex=0; rowIndex<= rowLength-1; rowIndex++){
+//            for(int colIndex = 0; colIndex<=colLength-1; colIndex++){
+//                System.out.print (brr[rowIndex][colIndex]+" ");
+//            }
+//            System.out.println();
+//        }
+
+
+
+        int[][] brr = {
+                {1, 2},
+                {2, 3,4,5},
+                {3, 4,5,6,7 },
+                {4}
+        };
 
 //        System.out.println(brr[3][1 ]);
         int rowLength = brr.length;        // for row length of 2d array
-        int colLength = brr[0].length;     // for col length of 2d array ,
-         // each row have same number of col so assume any one row length to get col length
+//        int colLength = brr[0].length;     // for col length of 2d array ,
+        // each row have same number of col so assume any one row length to get col length
 
         for(int rowIndex=0; rowIndex<= rowLength-1; rowIndex++){
+            // jaisa hi main kisi new row ma aaya
+            // same point par maine uss row ka colLength find kar liya
+            //current row -> brr[i] //because we dont know the index it might be anything
+            // isma kitna columns hai -> brr[rowIndex].length;
+            int colLength = brr[rowIndex].length;
             for(int colIndex = 0; colIndex<=colLength-1; colIndex++){
                 System.out.print (brr[rowIndex][colIndex]+" ");
             }
