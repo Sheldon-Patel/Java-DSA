4acad04338c2f2af88278e79db4f5f6733da8116
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-06-14 22:42:44 +0530

    initial

--- src/Lec13_Array/Arrays_2D.java
+++ src/Lec13_Array/Arrays_2D.java
@@ -1,4 +1,23 @@
 package Lec13_Array;
 
 public class Arrays_2D {
+
+    static void main(){
+
+    // decalartion
+    int[][] arr;
+    // allocation
+    arr =new int[3][4];
+    // init
+    int[][] brr = {
+                    {1, 2},
+                    {2, 3},
+                    {3, 4},
+                    {4, 5}
+                    };
+
+        System.out.println(brr[0][0]);
+
+
+}
 }
