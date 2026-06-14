3fa84dfcee66e2622f3ba7f68f10e934b518bb4b
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-06-14 23:12:57 +0530

    initial

--- src/Lec13_Array/Arrays_2D.java
+++ src/Lec13_Array/Arrays_2D.java
@@ -1,5 +1,7 @@
 package Lec13_Array;
 
+import javax.print.attribute.standard.OrientationRequested;
+
 public class Arrays_2D {
 
     static void main(){
@@ -30,7 +32,7 @@ public class Arrays_2D {
 
 
 
-        int[][] brr = {
+        int[][] brr = {     //also called jagged array because have different length
                 {1, 2},
                 {2, 3,4,5},
                 {3, 4,5,6,7 },
@@ -57,3 +59,26 @@ public class Arrays_2D {
 
 }
 }
+
+
+
+//homework: Row 0 columns: 2
+//Row 1 columns: 5
+//Row 2 columns: 10
+//Row 3 columns: 1
+//
+//nt[][] arr = new int[4][];
+//
+//arr[0] = new int[2];   // Row 0 -> 2 columns
+//arr[1] = new int[5];   // Row 1 -> 5 columns
+//arr[2] = new int[10];  // Row 2 -> 10 columns
+//arr[3] = new int[1];   // Row 3 -> 1 column
+//
+//         OR
+//
+//int[][] arr = {
+//        new int[2],
+//        new int[5],
+//        new int[10],
+//        new int[1]
+//};
