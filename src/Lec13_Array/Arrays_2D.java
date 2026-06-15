a6f8ca6322ee3e24ed01889f3ccf71b473c03ff0
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-06-15 23:00:31 +0530

    initial

--- src/Lec13_Array/Arrays_2D.java
+++ src/Lec13_Array/Arrays_2D.java
@@ -16,7 +16,8 @@ public class Arrays_2D {
         for (int i=0; i<= arr.length-1; i++){
             for (int j=0; j<= arr[i].length-1; j++){
                 int value= arr[i][j];
-                sum = sum+value;
+                sum = sum +value;
+//                sum = sum+arr[i][j];     // my method gives right answer
             }
         }
         System.out.println(sum);
