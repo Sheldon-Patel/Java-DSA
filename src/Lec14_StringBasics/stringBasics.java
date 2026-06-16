91d269d420e59e33a0c032f4344ad6cd9767f120
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-06-16 22:15:08 +0530

    initial

--- src/Lec14_StringBasics/stringBasics.java
+++ src/Lec14_StringBasics/stringBasics.java
@@ -1,9 +1,23 @@
 package Lec14_StringBasics;
 
+import java.util.Scanner;
+
 public class stringBasics {
     static void main() {
-        String name1 = "love";
-        String name2 = "love";
+        // string input
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Provide the string content: ");
+        String str = sc.nextLine();
+        System.out.println("Value of nextLine: "+ str);
+
+        System.out.println("Provide the string content: ");
+        String str2 = sc.next();
+        System.out.println("Value of next: "+ str2);
+
+
+
+//        String name1 = "love";
+//        String name2 = "love";
         // ==
 //        if (name1 == name2){
 //            System.out.println("string are equal");
@@ -19,12 +33,12 @@ public class stringBasics {
 //            System.out.println("string are not equal");
 //        }
         // .eqaulsIgnoreCase
-        if (name1.equalsIgnoreCase( name2) ){
-            System.out.println("string are equal");
-        }
-        else {
-            System.out.println("string are not equal");
-        }
+//        if (name1.equalsIgnoreCase( name2) ){
+//            System.out.println("string are equal");
+//        }
+//        else {
+//            System.out.println("string are not equal");
+//        }
 
 
 
