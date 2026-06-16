3c4d37f3320f6bf156a9a35a917ffb37b34595ee
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-06-16 21:57:45 +0530

    initial

--- src/Lec14_StringBasics/stringBasics.java
+++ src/Lec14_StringBasics/stringBasics.java
@@ -2,10 +2,15 @@ package Lec14_StringBasics;
 
 public class stringBasics {
     static void main() {
-       String firstName = "Love";
-       String lastName = new String("babbar");
-        System.out.println(firstName+" "+lastName );
-        System.out.println(firstName.length());
-        System.out.println(firstName.charAt(0));
+//       String firstName = "Love";
+//       String lastName = new String("babbar");
+//        System.out.println(firstName+" "+lastName );
+//        System.out.println(firstName.length());
+//        System.out.println(firstName.charAt(0));
+
+         String name =" Rana";
+         //name[0] = 'B';
+        name = "Bana";
+        System.out.println(name );
     }
 }
