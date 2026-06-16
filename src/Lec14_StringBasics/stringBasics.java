58512bfc5079c8b935866996424d3d5ba7986795
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-06-16 22:06:33 +0530

    initial

--- src/Lec14_StringBasics/stringBasics.java
+++ src/Lec14_StringBasics/stringBasics.java
@@ -2,15 +2,49 @@ package Lec14_StringBasics;
 
 public class stringBasics {
     static void main() {
+        String name1 = "love";
+        String name2 = "love";
+        // ==
+//        if (name1 == name2){
+//            System.out.println("string are equal");
+//        }
+//        else {
+//            System.out.println("string are not equal");
+//        }
+//        // .eqauls
+//        if (name1.equals(name2) ){
+//            System.out.println("string are equal");
+//        }
+//        else {
+//            System.out.println("string are not equal");
+//        }
+        // .eqaulsIgnoreCase
+        if (name1.equalsIgnoreCase( name2) ){
+            System.out.println("string are equal");
+        }
+        else {
+            System.out.println("string are not equal");
+        }
+
+
+
+
+
+
+
+
+
+
 //       String firstName = "Love";
 //       String lastName = new String("babbar");
 //        System.out.println(firstName+" "+lastName );
 //        System.out.println(firstName.length());
 //        System.out.println(firstName.charAt(0));
 
-         String name =" Rana";
-         //name[0] = 'B';
-        name = "Bana";
-        System.out.println(name );
+//        // string is immutable
+//         String name =" Rana";
+//         //name[0] = 'B';
+//        name = "Bana";
+//        System.out.println(name );
     }
 }
