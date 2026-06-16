4c08080646abf23b2d1bfd80ee33aaa71287475e
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-06-17 00:14:43 +0530

    initial

--- src/Lec14_StringBasics/stringBasics.java
+++ src/Lec14_StringBasics/stringBasics.java
@@ -53,6 +53,28 @@ public class stringBasics {
          return reverse;
     }
 
+    //practice
+    //5.check if palindrome
+    static boolean isPalindrome(String str){
+         String original=str;
+         String reverse = reverseString(original);
+         // compare
+        for(int i=0; i<original.length(); i++){
+            char ch1 = original.charAt(i);
+            char ch2 = reverse.charAt(i);
+            if (ch1 != ch2){
+                return false;
+
+            }
+        }
+        //loop se bhar tabhi aaunga
+        //jab saare character match krre hoga
+        // iska matlab return true
+        return true;
+    }
+
+
+
     static void main() {
         String str = "Love";
 //        printString(str );
@@ -61,7 +83,11 @@ public class stringBasics {
 
         System.out.println(getVowelCount(str));
 
-        System.out.println(getVowelCount(str));
+        System.out.println(reverseString(str));
+
+        System.out.println(isPalindrome(str));
+
+
 
 
 
