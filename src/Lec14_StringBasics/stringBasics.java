40ed83672cbebe0d53be01be9525265c2156ecfd
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-06-16 21:43:35 +0530

    initial

---
+++ src/Lec14_StringBasics/stringBasics.java
@@ -0,0 +1,9 @@
+package Lec14_StringBasics;
+
+public class stringBasics {
+    static void main() {
+       String firstName = "Love";
+       String lastName = new String("babbar");
+        System.out.println(firstName+" "+lastName );
+    }
+}
