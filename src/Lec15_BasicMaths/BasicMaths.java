5529283ccff4eba49858bdb84160697684c3608e
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-06-17 22:49:47 +0530

    initial

--- src/Lec15_BasicMaths/BasicMaths.java
+++ src/Lec15_BasicMaths/BasicMaths.java
@@ -2,17 +2,20 @@ package Lec15_BasicMaths;
 
 public class BasicMaths {
 
-    static void printDigits(int num){
+    static void printDigits(int num1){
         //agar mere num=0 ,tho main ruk jaunga
         //agar num!=0 tho main processing krta hu
 
-        while(num!=10){
-            int digit = num % 10;
+        while(num1!=0){
+            int digit = num1 % 10;
             System.out.println(digit);
+            // remove last digit
+            num1 = num1/10;
         }
     }
 
     static void main() {
-
+        int num = 53217;
+        printDigits(num);
     }
 }
