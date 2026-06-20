928439752e0b3c67122e73642b7f392692ceba5c
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-06-20 22:54:49 +0530

    initial

--- src/Lec16_BasicOOPS/oops.java
+++ src/Lec16_BasicOOPS/oops.java
@@ -1,5 +1,5 @@
 package Lec16_BasicOOPS;
 
 public class oops {
-    // only theory concept refer youtube video or diploma oop notes on drive or
+    // only theory concept refer youtube video or diploma oop notes on drive or whatsapp
 }
