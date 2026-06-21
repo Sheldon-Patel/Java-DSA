671ae2d36bcb21827b8b296e160ca8cb96b246e5
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-06-21 22:50:13 +0530

    initial

--- src/Lec17_CLASSandOBJECT/Student.java
+++ src/Lec17_CLASSandOBJECT/Student.java
@@ -3,5 +3,8 @@ package Lec17_CLASSandOBJECT;
 public class Student {
      // attribute
     public int id;
-    
+    public int age;
+    public String name;
+    public int nos;         //number of subject ->nos
+
 }
