a1993b54a2a1182d6386ba903ce28081d377f6e8
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-06-21 23:10:38 +0530

    initial

---
+++ src/Lec17_CLASSandOBJECT/App.java
@@ -0,0 +1,7 @@
+package Lec17_CLASSandOBJECT;
+
+public class App {
+    public static void main(String[] args) throws Exception{
+        
+    }
+}
--- src/Lec17_CLASSandOBJECT/Student.java
+++ src/Lec17_CLASSandOBJECT/Student.java
@@ -7,4 +7,21 @@ public class Student {
     public String name;
     public int nos;         //number of subject ->nos
 
+    //default constructor// attribute. ->garbage
+    public student(){
+        System.out.println(x:"Student Default constructor called");
+    }
+
+    //method or behaviours
+    public void study(){
+        System.out.println(name + "studying");
+    }
+
+    public void sleep(){
+        System.out.println(name + "sleeping");
+    }
+
+    public void bunk(){
+        System.out.println(name + "bunking");
+    }
 }
