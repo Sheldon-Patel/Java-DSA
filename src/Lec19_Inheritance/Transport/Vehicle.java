e48b08aab86dd6ede9135e6741da1ec81c648ffd
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-06-23 23:01:25 +0530

    initial

--- src/Lec19_Inheritance/Transport/Vehicle.java
+++ src/Lec19_Inheritance/Transport/Vehicle.java
@@ -1,4 +1,17 @@
 package Lec19_Inheritance.Transport;
 
 public class Vehicle {
+    public String name;
+
+    public String model;
+
+    public int noOfTyres;
+
+    Vehicle(String name, String model, int noOfTyres ){
+        this.name = name;
+        this.model = model;
+        this.noOfTyres = noOfTyres;
+    }
+
+
 }
