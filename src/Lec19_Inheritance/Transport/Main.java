66f0910e6f00e18b0305da8b8ee19eb9e28afbef
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-06-24 23:17:42 +0530

    initial

--- src/Lec19_Inheritance/Transport/Main.java
+++ src/Lec19_Inheritance/Transport/Main.java
@@ -1,4 +1,10 @@
 package Lec19_Inheritance.Transport;
 
 public class Main {
+    static void main(String[] args) {
+        Car c = new Car("Maruti","800",4,4,"Auto");
+        c.startEngine();
+        c.startAC();
+        c.stopEngine();
+    }
 }
