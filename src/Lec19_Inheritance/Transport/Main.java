25ba7c6414ccb193f61a4d4f6b1ec6cceae6d91c
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-06-24 23:25:01 +0530

    initial

--- src/Lec19_Inheritance/Transport/Main.java
+++ src/Lec19_Inheritance/Transport/Main.java
@@ -2,10 +2,10 @@ package Lec19_Inheritance.Transport;
 
 public class Main {
     static void main(String[] args) {
-//        Car c = new Car("Maruti","800",4,4,"Auto");
-//        c.startEngine();
-//        c.startAC();
-//        c.stopEngine();
+        Car c = new Car("Maruti","800",4,4,"Auto");
+        c.startEngine();
+        c.startAC();
+        c.stopEngine();
 
         MotorCycle m = new MotorCycle("splendor","Xline",2,"U","Soft");
         m.startEngine();
