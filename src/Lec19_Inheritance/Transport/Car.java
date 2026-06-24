3b11d8626411584a560b070659f5469a8dbbd1be
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-06-24 22:53:29 +0530

    initial

--- src/Lec19_Inheritance/Transport/Car.java
+++ src/Lec19_Inheritance/Transport/Car.java
@@ -2,4 +2,11 @@ package Lec19_Inheritance.Transport;
 
 public class Car extends Vehicle{
     public int noOfDoors;
+
+    public String transmissionType;
+
+    Car(String name, String model, int noOfTyres, int noOfDoors, String transmissionType){
+        super(name,model,noOfTyres);
+
+    }
 }
