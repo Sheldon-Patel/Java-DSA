bfb21a06c949e846411134045bb0f5bc804f4e3e
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-06-25 22:49:04 +0530

    initial

--- src/Lec20_Polymorphism/Main.java
+++ src/Lec20_Polymorphism/Main.java
@@ -2,10 +2,14 @@ package Lec20_Polymorphism;
 
 public class Main {
     static void main(String[] args) {
-        Calculator c = new Calculator();
-        System.out.println(c.add(2,3));
-        System.out.println(c.add(2,3,4));
-        System.out.println(c.add(2,3,4,5.6));
+//        Calculator c = new Calculator();
+//        System.out.println(c.add(2,3));
+//        System.out.println(c.add(2,3,4));
+//        System.out.println(c.add(2,3,4,5.6));
+
+
+        // runtime polymorphism
+
 
     }
 }
