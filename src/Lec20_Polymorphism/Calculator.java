ebf69ad1263a4b60b67eb455b37dbc87ed87f0fd
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-06-25 21:59:40 +0530

    initial

--- src/Lec20_Polymorphism/Calculator.java
+++ src/Lec20_Polymorphism/Calculator.java
@@ -7,6 +7,10 @@ public class Calculator {
 
     //overloading add, 3 int addition
     int add(int a, int b, int c) {
-        return a + b;
+        return a + b + c;
+    }
+
+    double add(int a, int b, int c, int d){
+        return a+b+c+d;
     }
 }
---
+++ src/Lec20_Polymorphism/Main.java
@@ -0,0 +1,8 @@
+package Lec20_Polymorphism;
+
+public class Main {
+    static void main(String[] args) {
+        Calculator c = new Calculator();
+        c.add(2,3)
+    }
+}
