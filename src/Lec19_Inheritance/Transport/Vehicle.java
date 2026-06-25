26bcc9c3da4a81dcd3e15171ba6eba75082f608d
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-06-25 20:47:12 +0530

    initial

--- src/Lec19_Inheritance/Transport/Vehicle.java
+++ src/Lec19_Inheritance/Transport/Vehicle.java
@@ -2,7 +2,7 @@ package Lec19_Inheritance.Transport;
 
 public class Vehicle {
      String name;
-     // nothing written so its
+     // nothing written so its default
 
     public String model;
 
