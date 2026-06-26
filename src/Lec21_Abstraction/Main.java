22f3355dc9c674f0d64165423950c618a4167b9d
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-06-26 22:43:06 +0530

    initial

--- src/Lec21_Abstraction/Main.java
+++ src/Lec21_Abstraction/Main.java
@@ -2,7 +2,26 @@ package Lec21_Abstraction;
 
 abstract class Bird{
     abstract void fly();
+
+    abstract void eat();
 }
+
+
+class sparrow implements Bird{
+
+    @Override
+    void fly() {
+        System.out.println();
+    }
+
+    @Override
+    void eat() {
+
+    }
+}
+
+
+
 
 public class Main {
 
