5038f1c451bb9a299197ed116c37f9a9597b762f
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-06-27 21:56:18 +0530

    initial

--- src/Lec21_Abstraction/Main.java
+++ src/Lec21_Abstraction/Main.java
@@ -1,13 +1,77 @@
+//package Lec21_Abstraction;
+//
+//abstract class Bird{
+//    abstract void fly();
+//
+//    abstract void eat();
+//}
+//
+//
+//class Sparrow extends Bird{
+//
+//    @Override
+//    void fly() {
+//        System.out.println("sparrow flying alag way sa");
+//    }
+//
+//    @Override
+//    void eat() {
+//        System.out.println("Sparrow eating");
+//    }
+//}
+//
+//class Crow extends Bird{
+//
+//    @Override
+//    void fly() {
+//        System.out.println("Crow flying");
+//    }
+//
+//    @Override
+//    void eat() {
+//        System.out.println("Crow eating alag way sa");
+//    }
+//}
+//
+//
+//
+//
+//public class Main {
+//
+//    static void doBirdStuff(Bird b){
+//        b.eat();
+//        b.fly();
+//    }
+//
+//    static void main(String[] args) {
+////        Bird b = new Sparrow();
+////        b.eat();
+////        b.fly();
+////
+////        b = new Crow();
+////        b.fly();
+////        b.eat();
+//        doBirdStuff(new Sparrow());
+//        doBirdStuff(new Crow());
+//
+//    }
+//
+//}
+
+
+
+//interface
 package Lec21_Abstraction;
 
-abstract class Bird{
-    abstract void fly();
+interface Bird{
+    void fly();
 
-    abstract void eat();
+    void eat();
+
 }
 
 
-class Sparrow extends Bird{
+class Sparrow implements Bird{
 
     @Override
     void fly() {
