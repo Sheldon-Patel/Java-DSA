295336614617124addd8dbeea7445ea48490abd2
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-06-30 22:40:44 +0530

    initial

--- src/Lec23_JavaCollectionFramework_1/Main.java
+++ src/Lec23_JavaCollectionFramework_1/Main.java
@@ -22,6 +22,14 @@ public class Main {
         list.remove(0);
         System.out.println(list);
 
+        //addAll
+        List<Integer> list2 = new ArrayList<>();
+        list2.add(101);
+        list2.add(102);
+
+        list.addAll(list2);
+        System.out.println(list);
+
 
 //        List<Integer> list = new ArrayList<>();
 //        Collection<Integer> collection =  new ArrayList<>();
