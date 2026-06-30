e3882ba5de8a40881a3b0df3ea7fc286945f5c69
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-06-30 22:27:48 +0530

    initial

--- src/Lec23_JavaCollectionFramework_1/Main.java
+++ src/Lec23_JavaCollectionFramework_1/Main.java
@@ -1,7 +1,21 @@
 package Lec23_JavaCollectionFramework_1;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 public class Main {
     static void main(String[] args) {
         // list or collection -> interface
+
+        // ArrayList -> concrete class
+        ArrayList<Integer> list = new ArrayList<>();
+
+        // add
+
+
+//        List<Integer> list = new ArrayList<>();
+//        Collection<Integer> collection =  new ArrayList<>();
+
     }
 }
