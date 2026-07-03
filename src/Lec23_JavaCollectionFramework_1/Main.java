372d525f7e2e13bab7fd6f54633fe640ad6c949b
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-07-03 22:46:48 +0530

    initial

--- src/Lec23_JavaCollectionFramework_1/Main.java
+++ src/Lec23_JavaCollectionFramework_1/Main.java
@@ -82,7 +82,7 @@ public class Main {
 
 
         ArrayList<Integer>newList = (ArrayList<Integer>) list.clone();
-        // (ArrayList<Integer>) is added
+        // (ArrayList<Integer>) is added for type
         System.out.println(newList);
     }
 }
