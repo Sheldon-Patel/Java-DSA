a1a98d798e5f7ab7a9b70c3fb01f9a74d6237bd1
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-07-05 23:38:48 +0530

    initial

--- src/Lec23_JavaCollectionFramework_1/Main.java
+++ src/Lec23_JavaCollectionFramework_1/Main.java
@@ -127,6 +127,10 @@ public class Main {
 
         System.out.println(ll.getLast());
 
+        System.out.println(ll.peek());
+
+
+
 //        ll.removeFirst();
 //        System.out.println(ll);
 //
