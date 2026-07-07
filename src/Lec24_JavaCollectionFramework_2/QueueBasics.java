11dda9bfa58cc90a92020c52f130869532f9c477
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-07-07 22:39:58 +0530

    initial

--- src/Lec24_JavaCollectionFramework_2/QueueBasics.java
+++ src/Lec24_JavaCollectionFramework_2/QueueBasics.java
@@ -6,7 +6,12 @@ import java.util.Queue;
 public class QueueBasics {
     public static void main(String[] args) {
         Queue<Integer> q = new LinkedList<>();
-            q.add(10);
+//            q.add(10);
+//            q.add(20);
+//            q.add(30);
+             q.offer(10);
+            q.offer(20);
+            q.offer(30);
 
 
     }
