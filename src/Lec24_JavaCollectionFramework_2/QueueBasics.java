0607b17b671092ba9acc10f51bda4476b989fdbc
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-07-07 22:37:32 +0530

    initial

--- src/Lec24_JavaCollectionFramework_2/QueueBasics.java
+++ src/Lec24_JavaCollectionFramework_2/QueueBasics.java
@@ -1,4 +1,13 @@
 package Lec24_JavaCollectionFramework_2;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class QueueBasics {
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+            q.add(10);
+
+
+    }
 }
