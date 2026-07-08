77f620611b408e5e33acd61f0f96cb1ca1f01191
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-07-08 22:51:30 +0530

    initial

--- src/Lec24_JavaCollectionFramework_2/HashSetsBasics.java
+++ src/Lec24_JavaCollectionFramework_2/HashSetsBasics.java
@@ -1,4 +1,42 @@
 package Lec24_JavaCollectionFramework_2;
 
+import java.util.HashSet;
+import java.util.Set;
+
+
 public class HashSetsBasics {
+    public static void main(String[] args){
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+        set1.add(4);
+
+        set2.add(3);
+        set2.add(4);
+        set2.add(5);
+        set2.add(6 );
+
+        System.out.println(set1);
+        set1.retainAll(set2);
+        System.out.println(set1);
+        System.out.println(set2);
+
+        System.out.println(set1.containsAll(set2));
+        System.out.println(set2.containsAll(set1));
+
+
+//        Set<Integer> st = new HashSet<>();
+//        st.add(10);
+//        st.add(10);
+//        st.add(10);
+//        st.add(10);
+//        st.add(10);
+//        st.add(20);
+//        st.add(20);
+//        st.add(30);
+//
+//        System.out.println(st);
+    }
 }
