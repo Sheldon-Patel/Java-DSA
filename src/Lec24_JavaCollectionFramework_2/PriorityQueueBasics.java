40638153b7975b50ae5580ad490d59993382fe8d
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-07-08 22:12:22 +0530

    initial

---
+++ src/Lec24_JavaCollectionFramework_2/PriorityQueueBasics.java
@@ -0,0 +1,16 @@
+package Lec24_JavaCollectionFramework_2;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class PriorityQueueBasics {
+     public static void main(String[] args) {
+         Queue<Integer> pq = new PriorityQueue<>();
+         pq.add(40);
+         pq.add(30);
+         pq.add(10);
+         pq.add(20);
+
+
+    }
+}
