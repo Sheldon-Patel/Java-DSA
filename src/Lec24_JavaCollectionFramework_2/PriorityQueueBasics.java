5daca8b85fae1284e8143e276099d3df76310f00
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-07-08 22:16:58 +0530

    initial

--- src/Lec24_JavaCollectionFramework_2/PriorityQueueBasics.java
+++ src/Lec24_JavaCollectionFramework_2/PriorityQueueBasics.java
@@ -6,11 +6,14 @@ import java.util.Queue;
 public class PriorityQueueBasics {
      public static void main(String[] args) {
          Queue<Integer> pq = new PriorityQueue<>();
-         pq.add(40);
-         pq.add(30);
-         pq.add(10);
-         pq.add(20);
+         pq.offer(40);
+         pq.offer(30);
+         pq.offer(10);
+         pq.offer(20);
 
-
+         System.out.println(pq);
+         System.out.println(pq.poll());
+         System.out.println(pq);
+          
     }
 }
