2e4bac326c1237750635a674bf44c2ecb7142e84
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-07-09 22:14:43 +0530

    initial

--- src/Lec25_JavaCollectionFramework_3/HashMapBasics.java
+++ src/Lec25_JavaCollectionFramework_3/HashMapBasics.java
@@ -1,4 +1,20 @@
 package Lec25_JavaCollectionFramework_3;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HashMapBasics {
+    public static void main(String[] args){
+
+        Map<String,String> mapping = new HashMap<>();
+
+        //insertion
+        mapping.put("in","India");
+        mapping.put("en","England");
+        mapping.put("us","United States");
+
+        System.out.println(mapping  );
+
+
+    }
 }
