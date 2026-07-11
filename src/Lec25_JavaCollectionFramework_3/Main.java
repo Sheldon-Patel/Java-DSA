b98ec76af0b4f84354c3c411e8352e0fdd51722f
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-07-11 21:20:16 +0530

    initial

---
+++ src/Lec25_JavaCollectionFramework_3/Main.java
@@ -0,0 +1,21 @@
+package Lec25_JavaCollectionFramework_3;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public class Main {
+    public static void main(String [] args){
+        List<Integer> list = new ArrayList<>();
+         list.add(15);
+         list.add(8);
+         list.add(2);
+         list.add(90);
+        System.out.println(list);
+
+        Collections.sort(list);
+        System.out.println(list);
+
+    }
+}
