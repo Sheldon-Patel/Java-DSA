182219e6b3f0847e9769f3434661a856a8af2007
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-07-11 23:06:43 +0530

    initial

--- src/Lec25_JavaCollectionFramework_3/ReverseCmp.java
+++ src/Lec25_JavaCollectionFramework_3/ReverseCmp.java
@@ -7,7 +7,7 @@ public class ReverseCmp implements Comparator<Integer> {
     @Override
     public int compare(Integer o1, Integer o2) {
         return 0 - Integer.compare(o1,o2);
-        return  - Integer.compare(o1,o2);
+//        return  - Integer.compare(o1,o2);
         //0- will make negative num to positive
         // it even work with only -
     }
