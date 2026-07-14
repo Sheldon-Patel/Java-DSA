aab550797f2e20495360e58e9ec3491f3b50fff5
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-07-14 23:48:51 +0530

    initial

--- src/Lec29_ArrayProblemSolving/Solution.java
+++ src/Lec29_ArrayProblemSolving/Solution.java
@@ -2,7 +2,22 @@ package Lec29_ArrayProblemSolving;
 
 // sort array of 0 ans 1
 public class Solution {
-    public int[] sortArray(int[] array){
+    public int[] sortArray(int[] nums){
 
+        int n = arr.length;
+        int i =0;
+        int j= n-1;
+
+        while(i<j){
+            if (arr[i]==1 && arr[j]==0){
+                //swap
+            }
+            if (arr[i]==0){
+                // increment i
+            }
+            if(arr[j==1]){
+                //decerement j
+            }
+        }
     }
 }
