79eaa78666c0b05f15009ae570f45e0ab777d626
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-07-15 21:42:13 +0530

    initial

--- src/Lec29_ArrayProblemSolving/Solution.java
+++ src/Lec29_ArrayProblemSolving/Solution.java
@@ -2,45 +2,45 @@ package Lec29_ArrayProblemSolving;
 
 // sort array of 0 ans 1
 public class Solution {
-    public int[] sortArray(int[] nums){
+    public int[] sortArray(int[] nums) {
 
         int n = nums.length;
-        int i =0;
-        int j= n-1;
+        int i = 0;
+        int j = n - 1;
 
-        while(i<j){
-            if (nums[i]==1 && nums[j]==0){
+        while (i < j) {
+            if (nums[i] == 1 && nums[j] == 0) {
                 //swap
-                nums[i]=0;
-                nums[j]=1;
+                nums[i] = 0;
+                nums[j] = 1;
 
             }
-            if (nums[i]==0){
+            if (nums[i] == 0) {
                 // increment i
                 i++;
 
             }
-            if(nums[j]==1){
+            if (nums[j] == 1) {
                 //decerement j
                 j--;
             }
         }
         return nums;
     }
-}
 
 
-public class Main {
-    public static void main(String[] args) {
+    //from gpt to run the code on ide
+        public static void main (String[]args){
 
-        Solution obj = new Solution();
+            int[] arr = {1, 0, 1, 0, 1, 0};
 
-        int[] nums = {0, 1, 1, 0, 1, 0};
+            Solution obj = new Solution();
 
-        int[] ans = obj.sortArray(nums);
+            int[] ans = obj.sortArray(arr);
 
-        for (int x : ans) {
-            System.out.print(x + " ");
+            for (int x : ans) {
+                System.out.print(x + " ");
+            }
         }
+
     }
-}
