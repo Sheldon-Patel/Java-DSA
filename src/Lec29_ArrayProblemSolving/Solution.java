268ac3dac8a3c618d5a51a8481404aa5a1b41adf
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-07-15 22:55:26 +0530

    initial

--- src/Lec29_ArrayProblemSolving/Solution.java
+++ src/Lec29_ArrayProblemSolving/Solution.java
@@ -1,46 +1,74 @@
 package Lec29_ArrayProblemSolving;
 
+
 // sort array of 0 ans 1
-public class Solution {
-    public int[] sortArray(int[] nums) {
 
+
+//time complexity: O(n)
+//space complexity: O(1)
+
+//public class Solution {
+//    public int[] sortArray(int[] nums) {
+//
+//        int n = nums.length;
+//        int i = 0;
+//        int j = n - 1;
+//
+//        while (i < j) {
+//            if (nums[i] == 1 && nums[j] == 0) {
+//                //swap
+//                nums[i] = 0;
+//                nums[j] = 1;
+//
+//            }
+//            if (nums[i] == 0) {
+//                // increment i
+//                i++;
+//
+//            }
+//            if (nums[j] == 1) {
+//                //decerement j
+//                j--;
+//            }
+//        }
+//        return nums;
+//    }
+//
+//
+//    //from gpt to run the code on ide
+//        public static void main (String[]args){
+//
+//            int[] arr = {1, 0, 1, 0, 1, 0};
+//
+//            Solution obj = new Solution();
+//
+//            int[] ans = obj.sortArray(arr);
+//
+//            for (int x : ans) {
+//                System.out.print(x + " ");
+//            }
+//        }
+//
+//    }
+
+
+
+// Missing value
+class Solution {
+    public int missingNumber(int[] nums) {
+        int xorSum = 0;
+
+        //xor with all the array element
+        for(int n:nums){
+            xorSum = xorSum ^ n;
+        }
+
+        //xor all the element in the range
         int n = nums.length;
-        int i = 0;
-        int j = n - 1;
-
-        while (i < j) {
-            if (nums[i] == 1 && nums[j] == 0) {
-                //swap
-                nums[i] = 0;
-                nums[j] = 1;
-
-            }
-            if (nums[i] == 0) {
-                // increment i
-                i++;
-
-            }
-            if (nums[j] == 1) {
-                //decerement j
-                j--;
-            }
-        }
-        return nums;
-    }
-
-
-    //from gpt to run the code on ide
-        public static void main (String[]args){
-
-            int[] arr = {1, 0, 1, 0, 1, 0};
-
-            Solution obj = new Solution();
-
-            int[] ans = obj.sortArray(arr);
-
-            for (int x : ans) {
-                System.out.print(x + " ");
-            }
+        for(int i= 0; i<= n; i++){
+            xorSum = xorSum ^ i;
         }
 
+        return xorSum;
     }
+}
