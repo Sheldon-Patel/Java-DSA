5f0019796d204c2658fbf036066a813e5556e5f9
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-07-19 22:11:33 +0530

    initial

--- src/Lec13_Array/Practice.java
+++ src/Lec13_Array/Practice.java
@@ -1,4 +1,22 @@
 package Lec13_Array;
 
 public class Practice {
+
+    // max value find out
+    static void main() {
+
+
+        int[] arr = {3, 6, 2, 9, 1};
+        int maxValue = arr[0];
+        int n = arr.length;
+
+        for (int i = 0; i <= n - 1; i++) {
+            if (arr[i] > maxValue) {
+                maxValue = arr[i];
+            }
+        }
+            System.out.println(maxValue);
+
+
+    }
 }
