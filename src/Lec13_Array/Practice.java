74418ceecb1f860b5b105213be1c22c595bee9cc
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-07-19 22:30:19 +0530

    initial

--- src/Lec13_Array/Practice.java
+++ src/Lec13_Array/Practice.java
@@ -1,7 +1,7 @@
 package Lec13_Array;
 
 for all the time complixity is O(n)
-            space complixity is O()
+            space complixity is O(1)
 public class Practice {
 
     static void main() {
