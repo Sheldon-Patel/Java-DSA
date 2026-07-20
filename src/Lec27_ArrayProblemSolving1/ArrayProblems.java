5cd43043147b2b4de8d87a9afa9ad4faf4c30029
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-07-20 22:23:05 +0530

    initial

--- src/Lec27_ArrayProblemSolving1/ArrayProblems.java
+++ src/Lec27_ArrayProblemSolving1/ArrayProblems.java
@@ -2,7 +2,10 @@ package Lec27_ArrayProblemSolving1;
 
 public class ArrayProblems {
     static double getAverage(int [] arr){
-        int sum = 0;
+//        int sum = 0;
+// we get o/p in double but sum is int so make it double instead of int or throught typecasting
+//        if you do one variable double java automaticaly divide in double
+        double sum = 0;
         for(int i:arr){
             sum = sum + i;
         }
@@ -12,7 +15,7 @@ public class ArrayProblems {
     }
 
     static void main() {
-    int arr[] ={2,4,3,3};
+    int arr[] ={2,4,1,3};
         System.out.println(getAverage(arr));
     }
 }
