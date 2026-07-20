a6c5a0bde1054be23766dcb146bb2609b103575b
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-07-20 23:31:46 +0530

    initial

--- src/Lec27_ArrayProblemSolving1/ArrayProblems.java
+++ src/Lec27_ArrayProblemSolving1/ArrayProblems.java
@@ -39,7 +39,7 @@ public class ArrayProblems {
     static void main() {
         int arr[]={1,2,3,4,5};
         int ans[] = multiplyBy10(arr);
-        System.out.println("printing arr ans");
+        System.out.println("printing arra ans");
         for (int i : ans){
             System.out.println(i);
         }
