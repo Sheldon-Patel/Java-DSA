4c05a0f39916403866c705107490e8cdba5b5472
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-07-20 22:19:02 +0530

    initial

--- src/Lec27_ArrayProblemSolving1/ArrayProblems.java
+++ src/Lec27_ArrayProblemSolving1/ArrayProblems.java
@@ -1,5 +1,18 @@
 package Lec27_ArrayProblemSolving1;
 
 public class ArrayProblems {
-    
+    static double getAverage(int [] arr){
+        int sum = 0;
+        for(int i:arr){
+            sum = sum + i;
+        }
+        int size = arr.length;
+        double avg = sum/size;
+        return avg;
+    }
+
+    static void main() {
+    int arr[] ={2,4,3,3};
+        System.out.println(getAverage(arr));
+    }
 }
