6f5cdd2918f6dbd36aae0ab4a1f52ee68b14568d
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-07-21 23:11:13 +0530

    initial

--- src/Lec27_ArrayProblemSolving1/ArrayProblems.java
+++ src/Lec27_ArrayProblemSolving1/ArrayProblems.java
@@ -40,7 +40,7 @@ public class ArrayProblems {
     static boolean findTarget(int [] arr, int target){
         for (int i=0; i<arr.length; i++){
             if (arr[i] == target){
-                return
+                return true;
             }
         }
     }
