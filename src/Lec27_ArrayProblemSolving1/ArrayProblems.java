96bff511dc252758089a7675193ddccbbaae6caf
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-08-05 22:24:49 +0530

    initial

--- src/Lec27_ArrayProblemSolving1/ArrayProblems.java
+++ src/Lec27_ArrayProblemSolving1/ArrayProblems.java
@@ -105,32 +105,36 @@ public class ArrayProblems {
 
 
     //return sum of positive and negative elements
-    static int[] getPosNegSum(int arr[]){
-        int posSum = 0;
-        int negSum = 0;
+//    static int[] getPosNegSum(int arr[]){
+//        int posSum = 0;
+//        int negSum = 0;
+//
+//        for (int i=0; i<arr.length;i++){
+//            if (arr[i] > 0){
+//                //num is positive
+//                posSum = posSum + arr[i];
+//            }
+//            else {
+//                //num is negative
+//                negSum = negSum + arr[i];
+//            }
+//        }
+//        int ans[]={posSum,negSum};
+//        return ans;
+//
+//    }
+//
+//    static void main() {
+//        int arr[]={2,-3,-5,-7,9};
+//        int ans[]=getPosNegSum(arr);
+//        System.out.println("positive sum: "+ans[0]);
+//        System.out.println("negative sum: "+ans[1]);
+//
+//    }
 
-        for (int i=0; i<arr.length;i++){
-            if (arr[i] > 0){
-                //num is positive
-                posSum = posSum + arr[i];
-            }
-            else {
-                //num is negative
-                negSum = negSum + arr[i];
-            }
-        }
-        int ans[]={posSum,negSum};
-        return ans;
 
-    }
+    // count the number of zero and one
 
-    static void main() {
-        int arr[]={2,-3,-5,-7,9};
-        int ans[]=getPosNegSum(arr);
-        System.out.println("positive sum: "+ans[0]);
-        System.out.println("negative sum: "+ans[1]);
-
-    }
 
 
     }
