230e0da737a0faaa138cc3093bff6f8aac032b17
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-08-05 22:25:33 +0530

    initial

--- src/Lec27_ArrayProblemSolving1/ArrayProblems.java
+++ src/Lec27_ArrayProblemSolving1/ArrayProblems.java
@@ -138,6 +138,14 @@ static int[] getZeroOneCount(int arr[]){
     int zeroCount = 0;
     int oneCount = 0;
 
+    for (int i = 0; i < arr.length; i++) {
+        if(arr[i] == 0){
+            zeroCount++;
+        }
+        else{
+            oneCount++;
+        }
+
 
 
 
