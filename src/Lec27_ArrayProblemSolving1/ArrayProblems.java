66d82b140d244bc68566ac46657690c00960c31d
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-08-10 22:19:12 +0530

    initial

--- src/Lec27_ArrayProblemSolving1/ArrayProblems.java
+++ src/Lec27_ArrayProblemSolving1/ArrayProblems.java
@@ -134,33 +134,38 @@ public class ArrayProblems {
 
 
     // count the number of zero and one
-static int[] getZeroOneCount(int arr[]){
-    int zeroCount = 0;
-    int oneCount = 0;
+//static int[] getZeroOneCount(int arr[]){
+//    int zeroCount = 0;
+//    int oneCount = 0;
+//
+//    for (int i = 0; i < arr.length; i++) {
+//        if(arr[i] == 0){
+//            zeroCount++;
+//        }
+//        else{
+//            oneCount++;
+//        }
+//
+//    }
+//    int ans[] ={zeroCount,oneCount};
+//    return ans;
+//}
+//
+//
+//    static void main(){
+//        int arr[] ={1,0,1,1,0,1,1};
+//        int ans[] = getZeroOneCount(arr);
+//        System.out.println("Zero count"+ans[0]);
+//        System.out.println("One count"+ans[1]);
+//    }
 
-    for (int i = 0; i < arr.length; i++) {
-        if(arr[i] == 0){
-            zeroCount++;
+
+
+    // find unsorted element in the array
+    static int getUnsortedElement(int arr[]){
+        for (int i=0; i< arr.length; i++){
+            if
         }
-        else{
-            oneCount++;
-        }
-
     }
-    int ans[] ={zeroCount,oneCount};
-    return ans;
-}
-
-
-    static void main(){
-        int arr[] ={1,0,1,1,0,1,1};
-        int ans[] = getZeroOneCount(arr);
-        System.out.println("Zero count"+ans[0]);
-        System.out.println("One count"+ans[1]);
-    }
-
-
-
-
     }
 
