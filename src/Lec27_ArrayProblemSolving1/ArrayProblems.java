24f8692b5890f3bbf84a45a293cfa2f8191d79cb
Sheldon-Patel <sheldonpatel98@gmail.com>
2026-08-12 22:35:22 +0530

    initial

--- src/Lec27_ArrayProblemSolving1/ArrayProblems.java
+++ src/Lec27_ArrayProblemSolving1/ArrayProblems.java
@@ -162,29 +162,50 @@ public class ArrayProblems {
 
 
     // find unsorted element in the array
-    static int getUnsortedElement(int arr[]) {
-        for (int i = 0; i < arr.length; i++) {
-//            if (arr[i + 1] > arr[i]) {
-//                // toh sub kuch thik hai
-//                // kuch karna ka need nahi hai
-//            } else {
-////                arr[i + 1] <= arr[i]
+//    static int getUnsortedElement(int arr[]) {
+//        for (int i = 0; i < arr.length; i++) {
+////            if (arr[i + 1] > arr[i]) {
+////                // toh sub kuch thik hai
+////                // kuch karna ka need nahi hai
+////            } else {
+//////                arr[i + 1] <= arr[i]
+////                return arr[i+1];
+////            }
+//            //right way to do this is
+//            if(arr[i+1]<=arr[i]){
 //                return arr[i+1];
 //            }
-            //right way to do this is
-            if(arr[i+1]<=arr[i]){
-                return arr[i+1];
-            }
+//        }
+//        //jis case ma se bhar ajaunga mean sorted loop which will not existed because it is said an unsorted elemnt
+//        //so to just remove error and we have used int to make function return -1
+//        return -1;
+//    }
+//
+//    static void main() {
+//        int arr[]={1,2,5,4,9};
+//        System.out.println(getUnsortedElement(arr));
+//    }
+
+
+    public static void swapAlternate(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i += 2) {
+            int temp = arr[i];
+            arr[i] = arr[i + 1];
+            arr[i + 1] = temp;
         }
-        //jis case ma se bhar ajaunga mean sorted loop which will not existed because it is said an unsorted elemnt
-        //so to just remove error and we have used int to make function return -1
-        return -1;
     }
 
-    static void main() {
-        int arr[]={1,2,5,4,9};
-        System.out.println(getUnsortedElement(arr));
-    }
+    public static void main(String[] args) {
 
+        int arr[] = {1, 2, 3, 4, 5, 6, 7};
+
+        swapAlternate(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
+}
+
+
 
